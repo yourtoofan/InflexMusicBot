e4a92dd8b651784883baef9b5d3851b29d8927ca
Legend mickey <toofanpapa424@gmail.com>
2024-11-12 11:20:07 +0530

    Update userbot.py

--- InflexMusic/core/userbot.py
+++ InflexMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamInflex")
-                await self.one.join_chat("InflexSupport")
+                await self.one.join_chat("The_INCRICIBLE")
+                await self.one.join_chat("ZOYU_Support")
             except:
                 pass
             assistants.append(1)
